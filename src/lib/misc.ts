9aedb5beb5ab2baf56fb654cd6793229ee7e6622
abdul <malik301002@gmail.com>
2023-03-24 22:56:01 +0700

    feat(lib): create misc and utils files

---
+++ src/lib/misc.ts
@@ -0,0 +1,20 @@
+const navList = [
+  {
+    title: 'Home',
+    href: '#home',
+  },
+  {
+    title: 'Services',
+    href: '#services',
+  },
+  {
+    title: 'Project',
+    href: '#project',
+  },
+  {
+    title: 'About us',
+    href: '#about-us',
+  },
+];
+
+export { navList };
