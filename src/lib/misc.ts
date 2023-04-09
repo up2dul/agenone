ad7a2f651e845756336cd4f29146e8dcca4d658e
abdul <malik301002@gmail.com>
2023-04-09 19:40:07 +0700

    feat: create Banner along with BannerItem component
    and static banner data

--- src/lib/misc.ts
+++ src/lib/misc.ts
@@ -17,4 +17,23 @@ const navList = [
   },
 ];
 
-export { navList };
+const bannerItems = [
+  {
+    title: 'Expanding the Market',
+    img: '/assets/icons/hero-expanding-the-market.svg',
+  },
+  {
+    title: 'Cost-Effective',
+    img: '/assets/icons/hero-cost-effective.svg',
+  },
+  {
+    title: 'More Professional',
+    img: '/assets/icons/hero-more-professional.svg',
+  },
+  {
+    title: 'Attract Client',
+    img: '/assets/icons/hero-attract-client.svg',
+  },
+];
+
+export { navList, bannerItems };
